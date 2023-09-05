bd11407c25666e448add9c5ad4d1a1fdc1b51f2a
skybound0532 <tianchenggu@outlook.com>
2023-09-05 14:16:02 -0500

    Work on transfer process

---
+++ console.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class console
+{
+    
+
+    public static void main(String[] args) throws IOException
+    {
+        RandomAccessFile in = new RandomAccessFile("textRFID.text", "r");
+
+        
+    }
+}
